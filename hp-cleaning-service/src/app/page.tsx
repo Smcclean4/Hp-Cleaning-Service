6a4c638f72a085e96bbfa6c075b61b4a22520240
Smcclean4 <lsaudim5@outlook.com>
2024-01-18 22:38:19 -0800

    update

--- hp-cleaning-service/src/app/page.tsx
+++ hp-cleaning-service/src/app/page.tsx
@@ -3,8 +3,8 @@ import { Navbar } from "./components/Navbar"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-6 bg-blue-400">
-      <div className="z-10 w-full items-center justify-between font-mono text-xl lg:flex">
-        <p>High Power Cleaning Service</p>
+      <div className="z-10 w-full items-center justify-between text-center font-mono text-xl lg:flex">
+        <p>High Power <br></br>Cleaning Service</p>
         <Navbar />
       </div>
     </main>
