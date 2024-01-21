6208e61b320df2225f632ab46ffda5096a2f0a21
Smcclean4 <lsaudim5@outlook.com>
2024-01-21 13:56:11 -0800

    updated meta and added some content

--- hp-cleaning-service/src/app/page.tsx
+++ hp-cleaning-service/src/app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import { Navbar } from "./components/Navbar"
 
 export default function Home() {
@@ -6,6 +7,10 @@ export default function Home() {
       <div className="z-10 w-full items-center justify-between text-left lg:text-center font-mono text-xl p-6 lg:flex bg-blue-500 text-white">
         <p>High Power <br></br>Cleaning Service</p>
         <Navbar />
+      </div>
+      <div className="flex justify-around items-center m-12 flex-col lg:flex-row lg:">
+        <p className=" text-2xl md:text-4xl font-mono text-blue-900 text-center m-10 w-full lg:w-1/2">Top Rated Cleaning Services! Years in the industry = Best quality service!</p>
+        <Image src={"/cleaning-service-3666011_1280.png"} alt={""} height="400" width="400"></Image>
       </div>
     </main>
   )
